a7908973b5b56550e5e58ec0c7d6136eeb42fecf
John Fawcett <jrf0110@gmail.com>
2013-05-09 15:16:14 -0500

    initial wlt component

---
+++ components/wlt/wlt.js
@@ -0,0 +1,12 @@
+define(function(require){
+  var
+    utils = require('utils')
+  , user  = require('user')
+  ;
+
+  return utils.View.extend({
+    className: 'btn-group wlt-btn-group'
+
+  ,
+  });
+});
