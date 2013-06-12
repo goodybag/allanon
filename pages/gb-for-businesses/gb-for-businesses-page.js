7a8f9183849a4967c8954d2f2d3e80b8d92688c1
John Fawcett <jrf0110@gmail.com>
2013-06-12 15:49:16 -0500

    finished gb for businesses

--- pages/gb-for-businesses/gb-for-businesses-page.js
+++ pages/gb-for-businesses/gb-for-businesses-page.js
@@ -13,7 +13,7 @@ define(function(require){
     className: 'page page-gb-for-businesses'
 
   , events: {
-
+      'submit #business-contact-form':      'onContactFormSubmit'
     }
 
   , initialize: function(){
@@ -23,6 +23,43 @@ define(function(require){
   , render: function(){
       this.$el.html( template() );
       return this;
+    }
+
+  , onContactFormSubmit: function(e){
+      e.preventDefault();
+
+      var this_ = this;
+
+      troller.spinner.spin();
+
+      utils.api.post(
+        'v1/businesses/contact-requests'
+
+      , {
+          name:         this.$el.find('.field-name').val()
+        , businessName: this.$el.find('.field-businessName').val()
+        , email:        this.$el.find('.field-email').val()
+        , zip:          +this.$el.find('.field-zip').val()
+        , comments:     this.$el.find('.field-comments').html()
+        }
+
+      , function(error){
+          troller.spinner.stop();
+          if (error) return troller.error(error, this_.$el);
+
+          this_.$el.find('input, textarea').each(function(i, el){
+            if ('value' in el) el.value = "";
+            else el.innerHTML = "";
+          });
+
+          var $title = this_.$el.find('.form-title');
+          var oldTitle = $title.html();
+
+          $title.text("Thanks! We got your message and we'll be contact shortly");
+
+          setTimeout(function(){ $title.html( oldTitle ) }, 5000);
+        }
+      );
     }
   });
 });
