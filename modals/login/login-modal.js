e955cca25b900578e726fdb66391546ffd369d23
Geoffrey Parker <grp@gparker.co>
2013-06-13 13:16:14 -0500

    login works, including remember me

--- modals/login/login-modal.js
+++ modals/login/login-modal.js
@@ -7,7 +7,11 @@ define(function(require) {
   return Components.Modal.Main.extend({
     className: 'modal hide fade modal-span4 login-modal',
 
-    events: {},
+    events: {
+      'click .facebook-login-button': 'oauth',
+      'submit .login-form':           'auth',
+      'click .forgot-password':       'forgotPassword'
+    },
 
     initialize: function(options) {
       Components.Modal.Main.prototype.initialize.apply(this, arguments);
@@ -19,13 +23,29 @@ define(function(require) {
       this.$el.html(template());
     },
 
+    completedLogin: function(err) {
+      if (err) return troller.error(err);
+      this.close();
+      // TODO: redirect to logged in state
+      window.location.reload();
+    },
+
     auth: function(e) {
+      // log in
+      var email = this.$el.find('.field-email').val();
+      var password = this.$el.find('.field-password').val();
+      var remember = this.$el.find('.remember-checkbox').is('checked');
+      // validation goes here
+      user.auth(email, password, remember, this.completedLogin);
     },
 
     oauth: function(e) {
+      // login with facebook
     },
 
     forgotPassword: function(e) {
+      // Dismiss this modal and call up the forgot password modal
+      this.close();
     }
   });
 });
