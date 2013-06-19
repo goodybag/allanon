c96fda93005e2d5c847168166f7981692b16aaa1
Geoffrey Parker <grp@gparker.co>
2013-06-19 16:53:15 -0500

    setup page display mechanics

--- pages/card-update/card-update-page.js
+++ pages/card-update/card-update-page.js
@@ -2,6 +2,7 @@ define(function(require){
   var utils       = require('utils');
   var config      = require('config');
   var user        = require('user');
+  var api         = require('api');
   var troller     = require('troller');
   var Components  = require('../../components/index');
 
@@ -15,17 +16,41 @@ define(function(require){
       'click confirm-card-update': 'submit'
     },
 
-    onOpen: function(options) {
+    onShow: function(options) {
       this.token = options.token;
+
+      var self = this;
+      troller.spinner.spin();
+      api.users.getCardUpdate(this.token, function(err, results) {
+        troller.spinner.stop();
+        //TODO: handle 404
+        if (err) {
+          troller.error(err.status === 404 ? 'bad token' : err);
+          return utils.history.navigate('/', {trigger: true});
+        }
+
+        if (results != null && results.data != null && typeof results.data.newCardId === 'string')
+          self.$newCardId.html(results.data.newCardId);
+      });
     },
 
     initialize: function(){
       this.model = user;
+      this.render();
+
+      this.$newCardId = this.$el.find('.new-card-id');
     },
 
     render: function(){
-      this.$el.html( template({oldCardId: this.model.get('cardId')}, ) )
+      this.$el.html( template({oldCardId: this.model.get('cardId') || "NONE"} ) )
       return this;
+    },
+
+    cancel: function(e) {
+
+    },
+
+    submit: function(e) {
     }
   });
 });
