604a8ca2daedb241c3ddcf12a63014945ae5d337
John Fawcett <jrf0110@gmail.com>
2013-07-09 10:12:44 -0500

    refactor slide stuff a little bit

--- components/pages/transitions.js
+++ components/pages/transitions.js
@@ -1,7 +1,46 @@
 define(function(require){
   var
     utils = require('utils')
+
   , transitions = {}
+
+  , helpers = {
+      slideA: function(view, toDirection, callback){
+        toDirection = utils.capitalize( toDirection );
+
+        // Get first-child's margin-top as that factors into the offsetheight
+        var top = view.$el.offset().top;
+        var $first = view.$el.find('> *:first-child');
+
+        if ($first.length && $first.css('margin-top'))
+          top -= parseInt($first.css('margin-top'));
+
+        top += 'px';
+
+        view.$el.css('position', 'absolute')
+        view.$el.css('top', top)
+        view.$el.addClass('pt-page-moveTo' + toDirection + 'Fade');
+        view.$el.one('webkitAnimationEnd mozAnimationEnd animationEnd', function(){
+          view.$el.css('display', 'none');
+          view.$el.css('position', "");
+          view.$el.removeClass('pt-page-moveTo' + toDirection + 'Fade');
+
+          if (callback) callback();
+        });
+      }
+
+    , slideB: function(view, fromDirection, callback){
+        fromDirection = utils.capitalize( fromDirection );
+
+        view.$el.css('display', 'block')
+        view.$el.addClass('pt-page-moveFrom' + fromDirection + 'Fade');
+        view.$el.one('webkitAnimationEnd mozAnimationEnd animationEnd', function(){
+          view.$el.removeClass('pt-page-moveFrom' + fromDirection + 'Fade');
+
+          if (callback) callback();
+        });
+      }
+    }
   ;
 
   transitions.none = function(viewA, viewB, callback){
@@ -23,60 +62,30 @@ define(function(require){
 
   transitions.slideToLeft = function(viewA, viewB, callback){
     var slideA = function(done){
-      var top = viewA.$el.offset().top;
-      var $first = viewA.$el.find('> *:first-child');
-      if ($first.length && $first.css('margin-top')) top -= parseInt($first.css('margin-top'));
-      top += 'px';
-      viewA.$el.css('position', 'absolute')
-      viewA.$el.css('top', top)
-      viewA.$el.addClass('pt-page-moveToLeftFade');
-      viewA.$el.one('webkitAnimationEnd mozAnimationEnd animationEnd', done);
+      helpers.slideA( viewA, 'Left', callback );
     };
 
     var slideB = function(done){
-      viewB.$el.css('display', 'block')
-      viewB.$el.addClass('pt-page-moveFromRightFade');
-      viewB.$el.one('webkitAnimationEnd mozAnimationEnd animationEnd', done);
+      helpers.slideB( viewB, 'Right', callback );
     };
 
     if ( !viewA ) return slideB( callback );
 
-    utils.parallel([ slideA, slideB ], function(){
-      viewA.$el.css('display', 'none');
-      viewA.$el.css('position', "")
-      viewA.$el.removeClass('pt-page-moveToLeftFade');
-      viewB.$el.removeClass('pt-page-moveFromRightFade');
-      if (callback) callback();
-    });
+    utils.parallel([ slideA, slideB ], callback);
   };
 
   transitions.slideToRight = function(viewA, viewB, callback){
     var slideA = function(done){
-      var top = viewA.$el.offset().top;
-      var $first = viewA.$el.find('> *:first-child');
-      if ($first.length && $first.css('margin-top')) top -= parseInt($first.css('margin-top'));
-      top += 'px';
-      viewA.$el.css('position', 'absolute')
-      viewA.$el.css('top', top)
-      viewA.$el.addClass('pt-page-moveToRightFade');
-      viewA.$el.one('webkitAnimationEnd mozAnimationEnd animationEnd', done);
+      helpers.slideA( viewA, 'Right', callback );
     };
 
     var slideB = function(done){
-      viewB.$el.css('display', 'block')
-      viewB.$el.addClass('pt-page-moveFromLeftFade');
-      viewB.$el.one('webkitAnimationEnd mozAnimationEnd animationEnd', done);
+      helpers.slideB( viewB, 'Left', callback );
     };
 
     if ( !viewA ) return slideB( callback );
 
-    utils.parallel([ slideA, slideB ], function(){
-      viewA.$el.css('display', 'none');
-      viewA.$el.css('position', "")
-      viewA.$el.removeClass('pt-page-moveToRightFade');
-      viewB.$el.removeClass('pt-page-moveFromLeftFade');
-      if (callback) callback();
-    });
+    utils.parallel([ slideA, slideB ], callback);
   };
 
   return transitions;
