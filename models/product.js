7ded33e0dc03481fa2c2ac45ced35c588674f242
Geoffrey Parker <grp@gparker.co>
2013-07-10 17:00:04 -0500

    indentation

--- models/product.js
+++ models/product.js
@@ -34,15 +34,15 @@ define(function(require){
     },
 
     defaults: {
-      price:      0,
-      tags:       [],
-      categories: [],
-      likes:      0,
-      wants:      0,
-      tries:      0,
-      userLikes:  false,
-      userWants:  false,
-      userTried:  false
+      price:       0,
+      tags:        [],
+      categories:  [],
+      likes:       0,
+      wants:       0,
+      tries:       0,
+      userLikes:   false,
+      userWants:   false,
+      userTried:   false
     },
 
     urlRoot: '/products',
